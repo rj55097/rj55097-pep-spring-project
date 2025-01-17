a94e07775fb8f42cd319ad2904369e4b28674e9d
Rose Jewel <roseajewel@gmail.com>
2025-01-17 05:27:54 +0000

    setting up repository and service

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,13 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.AccountRepository;
+
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,14 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.MessageRepository;
+
 public class MessageService {
+    MessageRepository messageRepository;
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+    
 }
